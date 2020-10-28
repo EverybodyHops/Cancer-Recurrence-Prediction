fe32c7f490e94fd635f261cb0fec0a1a48a32f82
朱子睿 <2016012240@secoder.net>
2020-10-28 13:39:06 +0800

    delete useless comment

--- read_data.py
+++ read_data.py
@@ -2,12 +2,6 @@ import pandas as pd
 import os, sys
 
 def read_raw_data(file_name, sheet_name="3月4日"):
-    """read raw data
-    Args:
-        file_name (str): path of data file
-        sheet_name (str): sheet name in excel
-    Returns: pandas dataframe of data
-    """
     _data = pd.read_excel(file_name, sheet_name=sheet_name)
     _data.drop(len(_data[_data.columns[0]]) - 1, inplace=True)
     return _data
