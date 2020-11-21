1a7d52d9dbb81d88012458f9685a215f7afac0ff
胡超臣 <hucc1949@gmail.com>
2020-11-21 16:57:41 +0800

    delete inappropriate feature

--- model.py
+++ model.py
@@ -8,12 +8,14 @@ parameters = {
                   
                   'metric': 'auc',         # 测试集使用的指标
                   
+                  'max_depth': 4,          # 树的最大深度
                   'num_leaves': 5,         # 叶子节点数
-                  'learning_rate': 0.2,  # 收敛速度
-                  'feature_fraction': 0.6, # 小于 1.0, LightGBM 将会在每次迭代中随机选择部分特征.
-                  'bagging_fraction': 0.6, # 类似于 feature_fraction, 但是它将在不进行重采样的情况下随机选择部分数据
-                  'bagging_freq': 6,       # bagging 的频率, 0 意味着禁用 bagging. k 意味着每 k 次迭代执行bagging
-                  'lambda_l1': 0.5,        # L1正则化
+                  'min_data_in_leaf': 20,  # 叶子节点上的数据的最小数量
+                  'learning_rate': 0.01,   # 收敛速度
+                  # 'feature_fraction': 0.6, # 小于 1.0, LightGBM 将会在每次迭代中随机选择部分特征.
+                  # 'bagging_fraction': 0.6, # 类似于 feature_fraction, 但是它将在不进行重采样的情况下随机选择部分数据
+                  # 'bagging_freq': 6,       # bagging 的频率, 0 意味着禁用 bagging. k 意味着每 k 次迭代执行bagging
+                  'lambda_l1': 0.08,        # L1正则化
                   }
 
 def train_model(data_train, label_train, data_valid, label_valid):
@@ -27,7 +29,7 @@ def train_model(data_train, label_train, data_valid, label_valid):
     gbm_model = lgb.train(parameters,
                     lgb_train,
                     valid_sets=[lgb_train, lgb_valid],
-                    num_boost_round=100,  # 提升迭代的次数
+                    num_boost_round=200,  # 提升迭代的次数
                     early_stopping_rounds=50,
                     evals_result=evals_result,
                     verbose_eval=50
@@ -47,10 +49,10 @@ def eval_model(data_test, label_test):
     
     print('Starting predicting...')
     # 模型预测
-    data_test_pred = gbm_model.predict(data_test, num_iteration=gbm_model.best_iteration)
+    label_test_pred = gbm_model.predict(data_test, num_iteration=gbm_model.best_iteration)
     
     # 模型评估
-    score = roc_auc_score(label_test, data_test_pred)
+    score = roc_auc_score(label_test, label_test_pred)
     
     print(score)
     print('Done!')
--- recurrence_prediction.py
+++ recurrence_prediction.py
@@ -7,8 +7,8 @@ import sys
 sys.argv.append(".\data.xls")
 
 Label_Index = -1
-Drop_Index = [39,40]
-Cat_Index = [0, 6, 7, 10, 11, 35, 37, 38]
+Drop_Index = [38, 39, 40]
+Cat_Index = [0, 6, 7, 10, 11, 35, 37]
 
 if __name__ == '__main__':
     
@@ -20,26 +20,26 @@ if __name__ == '__main__':
     for index in Drop_Index:
         data.drop([C_NAMES[index]], axis=1, inplace=True)
     for index in Cat_Index:
-        data[C_NAMES[index]] = data[C_NAMES[index]].astype('category')
+        pass
+        # data[C_NAMES[index]] = data[C_NAMES[index]].astype('category')
     
     data_model, data_test, label_model, label_test = train_test_split(data, label, test_size=0.2, random_state=2020)
 
-    #   五折交叉验证
+    # 五折交叉验证
     kf = KFold(n_splits=5, shuffle=True, random_state=2020)
     for train_index, valid_index in kf.split(data_model):
-        #   生成训练集和验证集
+        # 生成训练集和验证集
         data_train = data_model.copy()
         data_valid = data_model.copy()
         data_train.drop(data_train.index[valid_index], inplace=True)
         data_valid.drop(data_valid.index[train_index], inplace=True)
 
-        #   得到label
+        # 得到label
         label_train = label_model.drop(label_model.index[valid_index])
         label_valid = label_model.drop(label_model.index[train_index])
 
         gbm_model, evals_result = train_model(data_train, label_train, data_valid, label_valid)
-        # score = eval_model(data_valid, label_valid)
-
-
+        score = eval_model(data_valid, label_valid)
+    
     #  测试集评测
-    score = eval_model(data_test, label_test)
+    eval_model(data_test, label_test)
